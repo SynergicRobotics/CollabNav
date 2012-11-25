1318a3af6b7d7fc157f0e66b65108d94771988e4
Pablo Speciale <pablo.speciale@gmail.com>
2012-11-25 12:04:46 +0000

    Test in Osiris with 1..24 processors

--- collabnav_gmapping/gmapping_export/run_test.sh
+++ collabnav_gmapping/gmapping_export/run_test.sh
@@ -5,11 +5,12 @@
 function run_N_times {
   for i in {1..10}
   do
-    bin/gfs_nogui -filename data/nsh_level_a.log 2>/dev/null | grep -v 'filename'
+    bin/gfs_nogui -filename data/nsh_level_a.log 2>>output.txt | grep -v 'filename'
   done
 }
 
-for i in 1 2 4 8 12 16 20 24
+#for i in 1 2 4 8 12 16 20 24
+for i in {1..24}
 do
   export OMP_NUM_THREADS=$i
   echo -n "time_threads_$i = [ "
